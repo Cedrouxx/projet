87b1cfa2a55fc7463bbb9eca389bdfb428e4db41
Cedrouxx <cedrouxx-gamer@outlook.fr>
2021-08-20 14:29:57 +0000

    Auth

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+//models
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    public function register(){
+        return view('auth.register');
+    }
+    
+    public function registerPost(Request $request){
+        
+        $request->validate([
+            'username' => 'required|min:3|unique:users',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:8|confirmed'
+        ]);
+        
+        $user = new User();
+        $user->username = $request->input('username');
+        $user->email = $request->input('email');
+        $user->password = bcrypt($request->input('password'));
+        $user->is_admin = 0;
+        $user->save();
+        
+        return redirect()->route('auth.login.form');
+        
+    }
+    
+    public function login(){
+        return view('auth.login');
+    }
+    
+    public function loginPost(Request $request){
+        
+        $credentials = $request->validate([
+            'username' => 'required',
+            'password' => 'required'
+        ]);
+        
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+            return redirect()->intended(route('homepage'));
+        }
+        
+        return back()->withErrors([
+            'username' => 'Les identifiants ne correspondent pas',
+        ]);
+        
+    }
+    
+    public function logout(){
+        
+        Auth::logout();
+        
+        return redirect()->route('homepage');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
+//Controller
+use App\Http\Controllers\AuthController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +19,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// Auth
+Route::get('/register', [AuthController::class, 'register'])->name('auth.register.form');
+Route::post('/register', [AuthController::class, 'registerPost'])->name('auth.register.post');
+Route::get('/login', [AuthController::class, 'login'])->name('auth.login.form');
+Route::post('/login', [AuthController::class, 'loginPost'])->name('auth.login.post');
+Route::get('/logout', [AuthController::class, 'logout'])->name('auth.logout');
