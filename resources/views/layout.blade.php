c13c1d884e17ada5e266d339500eb0549a3e23cf
Cedrouxx <cedrouxx-gamer@outlook.fr>
2021-08-23 13:28:46 +0000

    navbar

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -11,30 +11,38 @@
     <header>
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">Game time</a>
+                <a class="navbar-brand" href="{{ route('homepage') }}">Game time</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Accueil</a>
+                            <a class="nav-link active" aria-current="page" href="{{ route('homepage') }}">Accueil</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Games</a>
+                            <a class="nav-link" href="{{ route('game.list') }}">Games</a>
+                        </li>
+                        @auth
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('auth.logout') }}">Déconnexion</a>
+                        </li>
+                        @else
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('auth.register.form') }}">Créer un compte</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Platforms</a>
+                            <a class="nav-link" href="{{ route('auth.login.form') }}">Connexion</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Studios</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Categories</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Authentication</a>
-                        </li>
+                        @endauth
+                        @if(isset(Auth::user()['is_admin']))
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('creator.game') }}">Ajouter un jeu</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('creator.studio') }}">Ajouter un studio</a>
+                            </li>
+                        @endif
                     </ul>
                 </div>
             </div>
